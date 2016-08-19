f496a3fdaf8a5dc6eeb3f08382f391e424ed2455
PreetiApshinge <preeti.2801@gmail.com>
2016-08-19 15:04:38 +0100

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,6 @@
+set PATH=%PATH%;C:/Program Files (x86)/Java/jdk1.7.0_51/bin;
+set CLASSPATH=%CLASSPATH%;C:/Program Files (x86)/Java/jdk1.7.0_51/jre/lib/rt.jar;C:/MyTest/selenium-server-standalone-2.32.0.jar;.
+
+javac Hotelbooking_PA.java
+
+java  Hotelbooking_PA
